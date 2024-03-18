7e2d9703f5fdd6a2ce91f8faa54bce2710d6ed80
loganrun <bigdog_41@hotmai.com>
2024-03-17 22:33:19 -0700

    grid added

--- src/components/NavaBar.jsx
+++ src/components/NavaBar.jsx
@@ -1,9 +1,9 @@
 
 function Navbar() {
     return (
-      <nav className="fixed top-0 left-0 h-screen w-full bg-gray-800 text-white"> {/* Adjust background and text color as needed */}
+      <nav className="fixed top-0 left-0  w-full bg-gray-800 text-white"> {/* Adjust background and text color as needed */}
         <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-          <h1 className="text-xl font-bold">DASBOARD</h1>
+          <h1 className="text-xl font-bold">Cryptify</h1>
           <ul className="hidden md:flex space-x-8"> {/* Adjust spacing for different screen sizes */}
             <li>
               <a href="#">Home</a>
