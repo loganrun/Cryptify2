b9e1567a143b9d584493a2db5e2ee670392e9726
loganrun <bigdog_41@hotmai.com>
2024-03-19 08:22:44 -0700

    cards working

--- src/components/CryptoCards.jsx
+++ src/components/CryptoCards.jsx
@@ -1,7 +1,7 @@
 
 function CryptoCards({ data }) {
-    // const { name, image, currentPrice, openingPrice, volume } = data;
-    // const priceDifference = currentPrice - openingPrice;
+    const { name, image, current_price, price_change_24h, total_volume } = data;
+    //const priceDifference = current_price - openingPrice;
   
     return (
       <div className="bg-[#423A6F] rounded-lg shadow-md overflow-hidden">
@@ -10,14 +10,14 @@ function CryptoCards({ data }) {
           <img className="w-8 h-8 rounded-full" src={image} alt={name} />
         </div>
         <div className="px-4 py-2">
-          <p className="text-gray-600">Current Price: ${currentPrice.toFixed(2)}</p>
-          <p className="text-gray-600">Opening Price: ${openingPrice.toFixed(2)}</p>
-          {priceDifference > 0 ? (
-            <p className="text-green-500">Difference: +${priceDifference.toFixed(2)}</p>
+          <p className="text-gray-600">Current Price: ${current_price.toFixed(2)}</p>
+          <p className="text-gray-600"> Last 24 Hours: ${price_change_24h.toFixed(2)}</p>
+          {price_change_24h > 0 ? (
+            <p className="text-green-500">Difference: +${price_change_24h.toFixed(2)}</p>
           ) : (
-            <p className="text-red-500">Difference: -${Math.abs(priceDifference).toFixed(2)}</p>
+            <p className="text-red-500">Difference: -${Math.abs(price_change_24h).toFixed(2)}</p>
           )}
-          <p className="text-gray-600">Volume: ${volume.toFixed(2)}</p>
+          <p className="text-gray-600">Volume: ${total_volume.toFixed(2)}</p>
         </div>
       </div>
     );
