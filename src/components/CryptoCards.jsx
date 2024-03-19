eea4f2eb8ce7f3ee7557047374a2e646cf69de3a
loganrun <bigdog_41@hotmai.com>
2024-03-19 08:30:44 -0700

    revised

--- src/components/CryptoCards.jsx
+++ src/components/CryptoCards.jsx
@@ -5,19 +5,19 @@ function CryptoCards({ data }) {
   
     return (
       <div className="bg-[#423A6F] rounded-lg shadow-md overflow-hidden">
-        <div className="flex items-center px-4 py-4">
+        <div className="flex items-center px-4 py-4 justify-between">
           <h3 className="text-lg font-bold mr-4">{name}</h3>
           <img className="w-8 h-8 rounded-full" src={image} alt={name} />
         </div>
         <div className="px-4 py-2">
-          <p className="text-gray-600">Current Price: ${current_price.toFixed(2)}</p>
-          <p className="text-gray-600"> Last 24 Hours: ${price_change_24h.toFixed(2)}</p>
+          <p className="text-white">Current Price: ${current_price.toFixed(2)}</p>
+          <p className="text-white"> Last 24 Hours: ${price_change_24h.toFixed(2)}</p>
           {price_change_24h > 0 ? (
             <p className="text-green-500">Difference: +${price_change_24h.toFixed(2)}</p>
           ) : (
             <p className="text-red-500">Difference: -${Math.abs(price_change_24h).toFixed(2)}</p>
           )}
-          <p className="text-gray-600">Volume: ${total_volume.toFixed(2)}</p>
+          <p className="text-white">Volume: {total_volume.toFixed(2)}</p>
         </div>
       </div>
     );
